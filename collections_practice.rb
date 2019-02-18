e919190868e19c2c425ec8ba35674526f0a2f70e
Fillan Lee <fillanlee@aol.com>
2019-02-18 19:56:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
 # your code goes here
+def begins_with_r(array)
+array.all? do |v|
+  v.start_with?("r")
+end
+end
+  
+
+
+def contain_a(array)
+  container = []
+  array.each do |v|
+    if v.include?("a")
+      
+    end
+  end
+end
+
+def first_wa(array)
+  array.detect do |word|
+    word.
+  
+  
+  
+end
+def remove_non_strings
+  
+  
+end
+def count_elements
+  
+  
+end
+def merge_data
+  
+  
+end
+def find_cool
+  
+  
+end
